9ea306e77ed911b6b2d6ad75ce209b4d98d18c21
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-02-16 12:56:51 +0530

    share data using @Input

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServerElementComponent implements OnInit {
 
   // define element type
-  element: {name: string, type: string, content: string};
+  @Input() element: {name: string, type: string, content: string};
 
   constructor() { }
 
