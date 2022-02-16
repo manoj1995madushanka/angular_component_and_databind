127a4d758059f3f42c4c2a8909a0ed6f1865ae91
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-02-16 14:55:18 +0530

    view encapsulate of css style

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,16 +1,21 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  /* below line will disable view encapsulation of server.element.css file
+  * that means those file css properties will apply to whole app that classes*/
+  encapsulation: ViewEncapsulation.None
 })
 export class ServerElementComponent implements OnInit {
 
   // define element type
-  @Input('srvElement') element: {name: string, type: string, content: string};
+  // tslint:disable-next-line:no-input-rename
+  @Input('srvElement') element: { name: string, type: string, content: string };
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
